7d44b8c45998202b4981a8a126a1138449c79c1e
kecskesdavid <david.kecskes@electrolux.com>
2022-07-16 11:06:35 +0300

    add and secure API key

--- app/src/main/java/com/example/searchappcompose/data/remote/consts/Constants.kt
+++ app/src/main/java/com/example/searchappcompose/data/remote/consts/Constants.kt
@@ -2,4 +2,6 @@ package com.example.searchappcompose.data.remote.consts
 
 object Constants {
     const val BASE_URL = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/"
+    const val X_RAPIDAPI_KEY = "X-RapidAPI-Key"
+    const val X_RAPIDAPI_HOST = "X-RapidAPI-Host"
 }
--- app/src/main/java/com/example/searchappcompose/data/remote/di/NetworkModule.kt
+++ app/src/main/java/com/example/searchappcompose/data/remote/di/NetworkModule.kt
@@ -1,21 +1,42 @@
 package com.example.searchappcompose.data.remote.di
 
+import com.example.searchappcompose.BuildConfig
 import com.example.searchappcompose.data.remote.consts.Constants
 import com.example.searchappcompose.data.remote.service.WebSearchApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
-import kotlin.reflect.jvm.internal.impl.load.java.Constant
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val okHttpBuilder = OkHttpClient.Builder()
+
+//        okHttpBuilder.readTimeout(READ_TIME_OUT, TimeUnit.SECONDS)
+//        okHttpBuilder.connectTimeout(CONNECT_TIME_OUT, TimeUnit.SECONDS)
+        okHttpBuilder.addInterceptor(Interceptor { chain ->
+
+            val original = chain.request().newBuilder().apply {
+                addHeader(Constants.X_RAPIDAPI_KEY, BuildConfig.X_RAPIDAPI_KEY_SECRET)
+                addHeader(Constants.X_RAPIDAPI_HOST, BuildConfig.X_RAPIDAPI_HOST_SECRET)
+            }
+
+            chain.proceed(original.build())
+        })
+
+        return okHttpBuilder.build()
+    }
 
     @Provides
     @Singleton
