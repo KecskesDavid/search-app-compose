e96362c3e73527605dcc0dd363c674e0a98d411d
kecskesdavid <david.kecskes@electrolux.com>
2022-07-17 17:42:06 +0300

    change repository injection to binding

--- app/src/main/java/com/example/searchappcompose/data/remote/di/RepositoryModule.kt
+++ app/src/main/java/com/example/searchappcompose/data/remote/di/RepositoryModule.kt
@@ -1,21 +1,20 @@
 package com.example.searchappcompose.data.remote.di
 
 import com.example.searchappcompose.data.remote.repository.NewsRepositoryImpl
-import com.example.searchappcompose.data.remote.service.WebSearchApi
 import com.example.searchappcompose.domain.repository.NewsRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideNewsRepository(webSearchApi: WebSearchApi): NewsRepository {
-        return NewsRepositoryImpl(webSearchApi)
-    }
+    abstract fun bindNewsRepository(
+        newsRepositoryImpl: NewsRepositoryImpl
+    ): NewsRepository
 }
--- app/src/main/java/com/example/searchappcompose/data/remote/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/searchappcompose/data/remote/repository/NewsRepositoryImpl.kt
@@ -4,8 +4,9 @@ import com.example.searchappcompose.data.remote.mappers.toNews
 import com.example.searchappcompose.data.remote.service.WebSearchApi
 import com.example.searchappcompose.domain.model.news.News
 import com.example.searchappcompose.domain.repository.NewsRepository
+import javax.inject.Inject
 
-class NewsRepositoryImpl(
+class NewsRepositoryImpl @Inject constructor(
     private val webSearchApi: WebSearchApi
 ) : NewsRepository {
 
