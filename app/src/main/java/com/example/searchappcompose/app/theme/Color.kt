98f4992746ec105bba85f9bbb725a111f597984c
kecskesdavid <kecskesdavid7@gmail.com>
2023-01-05 18:18:03 +0200

    Resolve issues with top navigation bar and theme

--- app/src/main/java/com/example/searchappcompose/app/theme/Color.kt
+++ app/src/main/java/com/example/searchappcompose/app/theme/Color.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Primary = Color(0xFF94B49F)
 val Secondary = Color(0xFFCEE5D0)
 val Tertiary = Color(0xFFFCF8E8)
+val Background = Color(0xFF5C7A66)
 
 val PrimaryDark = Color(0xFF3F4E4F)
 val SecondaryDark = Color(0xFFA27B5C)
--- app/src/main/java/com/example/searchappcompose/app/theme/Theme.kt
+++ app/src/main/java/com/example/searchappcompose/app/theme/Theme.kt
@@ -1,18 +1,14 @@
 package com.example.searchappcompose.app.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
@@ -23,7 +19,7 @@ private val DarkColorScheme = darkColorScheme(
     background = BackgroundDark,
     surface = BackgroundDark,
     onSurface = White,
-    onBackground = Color.Gray,
+    onBackground = White,
     outline = SecondaryDark
 )
 
@@ -31,9 +27,9 @@ private val LightColorScheme = lightColorScheme(
     primary = Primary,
     secondary = Secondary,
     tertiary = Tertiary,
-    background = White,
+    background = Background,
     surface = White,
-    onBackground = Color.Gray,
+    onBackground = White,
     onSurface = Black,
     outline = Secondary
 )
@@ -44,12 +40,8 @@ fun SearchAppComposeTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
+    val colorScheme = when (darkTheme) {
+        true -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
